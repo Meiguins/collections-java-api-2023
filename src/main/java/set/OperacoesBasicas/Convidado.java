000445c038181bc8a84dd58e1a93e67388075b1a
Kauan Meiguins <kauanmeiguins3@gmail.com>
2023-10-03 19:59:04 -0300

    Resolvido 3

--- src/main/java/set/OperacoesBasicas/Convidado.java
+++ src/main/java/set/OperacoesBasicas/Convidado.java
@@ -1,4 +1,27 @@
 package set.OperacoesBasicas;
 
 public class Convidado {
+    private String nome;
+    private int codigoConvidado;
+
+    public Convidado(String nome, int codigoConvidado) {
+        this.nome = nome;
+        this.codigoConvidado = codigoConvidado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvidado() {
+        return codigoConvidado;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvidado=" + codigoConvidado +
+                '}';
+    }
 }
